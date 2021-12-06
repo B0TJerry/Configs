38103e32b4b14f09d850a347a3c9673675438768
Jerry Yurek <g3yurek@pm.me>
2021-12-06 13:31:17 -0500

    added things

--- Laptop/.config/fish/config.fish
+++ Laptop/.config/fish/config.fish
@@ -53,18 +53,19 @@ set fish_cursor_visual block
 
 #set -gx PAGER 'less --wheel-lines=4 --mouse --use-color -Dd+b -Du+g -DE+r -DS+ky -J -N -M'
 
-set -gx PAGER "bat --style=plain"
-set -Ux EDITOR "nvim"
+set -gx PAGER "bat -p"
+set -gx EDITOR "nvim"
 
 # NNN settings
 set -gx NNN_TMPFILE "/tmp/nnn"
 set -gx NNN_FIFO "/tmp/nnn.fifo"
-set -gx NNN_PLUG "f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;l:preview-tui;i:imgview"
+set -gx NNN_PLUG "f:finder;o:fzopen;p:mocplay;d:diffs;t:nmount;l:preview-tui;i:imgview;g:getplugs;e:suedit;z:zathura"
+set -gx NNN_OPENER "nvim"
 set -gx BLK "04"
 set -gx CHR "04"
 set -gx DIR "04"
-set -gx EXE "00"
-set -gx REG "00"
+set -gx EXE "04"
+set -gx REG "07"
 set -gx HARDLINK "00"
 set -gx SYMLINK "06"
 set -gx MISSING "00"
@@ -74,4 +75,30 @@ set -gx SOCK "0F"
 set -gx OTHER "02"
 set -gx NNN_FCOLORS "$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
 
+#Use zoxide as a cd replacement
+zoxide init fish --cmd cd | source
 
+#dvtm settings
+set -gx DVTM_EDITOR = "nvim"
+
+#open tmux automatically
+#
+if [ $TERM = "linux" ]; then
+    echo -en "\e]P0282828" #black
+    echo -en "\e]P8928374" #darkgrey
+    echo -en "\e]P1cc241d" #darkred
+    echo -en "\e]P9fb4934" #red
+    echo -en "\e]P298971a" #darkgreen
+    echo -en "\e]PAb8bb26" #green
+    echo -en "\e]P3d79921" #brown
+    echo -en "\e]PBfabd2f" #yellow
+    echo -en "\e]P4458588" #darkblue
+    echo -en "\e]PC83a598" #blue
+    echo -en "\e]P5b16286" #darkmagenta
+    echo -en "\e]PDd3869b" #magenta
+    echo -en "\e]P6689d6a" #darkcyan
+    echo -en "\e]PE8ec07c" #cyan
+    echo -en "\e]P7a89984" #lightgrey
+    echo -en "\e]PFebdbb2" #white
+    clear #for background artifacting
+end
